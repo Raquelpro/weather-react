0dce96e10ab09078aed1fe4ce4a0af97382a3724
Raquelpro <107865288+Raquelpro@users.noreply.github.com>
2022-10-13 09:54:02 -0500

    Added api to project

--- src/Weather.js
+++ src/Weather.js
@@ -1,80 +1,103 @@
-import React from "react";
+import React, { useState} from "react";
+import axios from "axios";
 import "./App.css";
 
-export default function Weather() {
-    return (
-        <div className="Weather">
-        <div className="container">
-          <div className="weather-app-wrapper">
-            <div className="weather-app">
-              <form className="mb-5">
-                <div className="row">
-                  <div className="col-9">
-                    <input
-                      type="search"
-                      placeholder="Type a city.."
-                      className="form-control"
-                    />
-                  </div>
-                  <div className="col-3">
-                    <input
-                      className="btn btn-primary"
-                      type="submit"
-                      value="Submit"
-                    />
-                  </div>
-                </div>
-              </form>
-              <div className="container text-center">
-                <div className="col-md-6 offset-md-3">
-                  <h1>Dallas Texas</h1>
-                  <ul>
-                    <li>
-                      Last updated at: <span>Monday</span>
-                    </li>
-                    <li></li>
-                  </ul>
-                </div>
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ready: false});
+function handleResponse(response) {
+  setWeatherData({
+    ready: true,
+    temperature: response.data.main.temp,
+    humidity: response.data.main.humidity,
+    date: "Wednesday 07:00",
+    description: response.data.weather[0].description,
+    iconUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+    wind: response.data.wind.speed,
+    city: response.data.name
+  });
+}
+
+if (weatherData.ready) {
+  return (
+    <div className="Weather">
+    <div className="container">
+      <div className="weather-app-wrapper">
+        <div className="weather-app">
+          <form className="mb-5">
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="Type a city.."
+                  className="form-control"
+                />
               </div>
-              <div className="row">
-                <div className="col-md-4 offset-md-4">
-                  <div className="clearfix weather-temperature">
-                    <img
-                      src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-                      alt="clear"
-                      id="icon"
-                    />
-                    <strong></strong>
-                    <span className="units">
-                      <a href="/">°C</a> |<a href="/">°F</a>
-                    </span>
-                  </div>
-                </div>
-  
-                <div className="col-md-3 offset-md-3">
-                  <ul>
-                    <li>Precipitation: 0%</li>
-                    <li>
-                      Humidity: <span>80%</span>%
-                    </li>
-                    <li>
-                      Wind: <span>35</span>km/hr
-                    </li>
-                  </ul>
-                </div>
+              <div className="col-3">
+                <input
+                  className="btn btn-primary"
+                  type="submit"
+                  value="Submit"
+                />
               </div>
             </div>
-            <br />
-            <div className="weather-forcast"></div>
-            <div className="row"></div>
-            <small>
-              <a href="https://github.com/Raquelpro/weather-react" target="_blank" rel="noreferrer">
-                Open-source code
-              </a>
-              , by Raquel Provvidente
-            </small>
+          </form>
+          <div className="container text-center">
+            <div className="col-md-6 offset-md-3">
+              <h1>{weatherData.city}</h1>
+              <ul>
+                <li>
+                  Last updated at: <span>{weatherData.date}</span>
+                </li>
+                <li className="text-capitalize">{weatherData.description}</li>
+              </ul>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4 offset-md-4">
+              <div className="clearfix weather-temperature">
+                <img
+                  src={weatherData.iconUrl}
+                  alt={weatherData.description}
+                  id="icon"
+                />
+                <strong></strong>
+                <span className="units">{Math.round(weatherData.temperature)}
+                  <a href="/">°C</a> |<a href="/">°F</a>
+                </span>
+              </div>
+            </div>
+
+            <div className="col-md-3 offset-md-3">
+              <ul>
+                <li>Precipitation: 0%</li>
+                <li>
+                  Humidity: <span>{weatherData.humidity}</span>
+                </li>
+                <li>
+                  Wind: <span>{weatherData.wind}</span>km/hr
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>  
-    )
+        <br />
+        <div className="weather-forcast"></div>
+        <div className="row"></div>
+        <small>
+          <a href="https://github.com/Raquelpro/weather-react" target="_blank" rel="noreferrer">
+            Open-source code
+          </a>
+          , by Raquel Provvidente
+        </small>
+      </div>
+    </div>
+  </div>  
+);
+} else {
+  const apiKey = "3ed26e20a2e7286d23126eb51e31f310";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);  
+
+  return "Loading...";
+}  
 }
